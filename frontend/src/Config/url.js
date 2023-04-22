8ca413218d94ae0f490ed6a92bc25e20ad407f6a
apkharsh@gmail.com <apkharsh@gmail.com>
2023-04-22 15:27:44 +0530

    minor changes

--- frontend/src/Config/url.js
+++ frontend/src/Config/url.js
@@ -1,2 +1,2 @@
-// export const BASE_URL = "https://hotel-manage-zr8f.onrender.com/api";
-export const BASE_URL = "http://localhost:5000/api";
+export const BASE_URL = "https://hotel-manage-zr8f.onrender.com/api";
+// export const BASE_URL = "http://localhost:5000/api";
